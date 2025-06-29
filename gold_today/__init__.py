6b68f264cec6da3ef7dcca84f82632f93d615ba5
Haeder Ali <[masaleedesign@gmail.com]>
2025-06-29 11:10:36 +0700

    Update beautifulsoup

--- gold_today/__init__.py
+++ gold_today/__init__.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 def data_extraction():
     """
     Harga Emas Hari Ini, 28 Jun 2025
@@ -22,23 +25,36 @@ def data_extraction():
     1000 gr	1,824,600,000	1,829,161,500
     :return:
     """
+    try:
+        content = requests.get('https://www.lakuemas.com/')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text,'html.parser')
+        update_gold_today = soup.find('small', {'style': 'letter-spacing: 1px !important; font-size: 14px;'})
 
-    r = dict()
-    r['update gold today'] = 'June, 28 2025'
-    r['prices are update daily at'] = '08:30 WIB'
-    r['weight'] = ('0.5 gr','1 gr', '2 gr', '3 gr', '4 gr', '5 gr', '10 gr', '25 gr','50 gr',
+
+        r = dict()
+        r['update gold today'] = update_gold_today.text # 'June, 28 2025'
+        r['prices are update daily at'] = '08:30 WIB'
+        r['weight'] = ('0.5 gr','1 gr', '2 gr', '3 gr', '4 gr', '5 gr', '10 gr', '25 gr','50 gr',
                    '100 gr','250 gr','500 gr','1000 gr')
-    r[
+        r[
         "base price IDR"] = ('992,000','1,884,000','3,708,000','5,537,000','9,195,000','18,335,000',
                              '45,712,000','91,345,000','182,612,000','456,265,000','912,320,000',
                              '1,824,600,000')
-    r['price +pph 0.25% IDR'] = (
+        r['price +pph 0.25% IDR'] = (
         '994,480','1,888,710','3,717,270','5,550,843','9,217,988','18,380,838','45,826,280','91,573,363',
                                  '183,068,530','457,405,663','914,600,800','1,829,161,500')
-    return r
+        return r
+    else:
+        return None
 
 def view_data(result):
-    print('Update Price Gold Today source logammulia.com')
+    if result is None:
+        print('Data not found!')
+        return
+    print('Update Price Gold Today source lakuemas.com')
     print('\n')
     print(f"Update gold today {result['update gold today']}")
     print(f"prices are update daily at {result['prices are update daily at']}")
